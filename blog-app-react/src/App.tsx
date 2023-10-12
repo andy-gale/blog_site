d5a14bf44fc94a1af7adddf5e9fbbc7f83e690c7
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2023-10-11 20:48:25 -0700

    basic blog layout in html and template react components

--- blog-app-react/src/App.tsx
+++ blog-app-react/src/App.tsx
@@ -1,6 +1,7 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import Navbar from "./components/Navbar";
 import { useState } from "react";
 
 function App() {
@@ -13,27 +14,38 @@ function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
-    <div>
-      //each component has independent states
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-      <div>
-        {alertVisible && (
-          <Alert onClose={() => setAlertVisibility(false)}>Alert1!</Alert>
-        )}
-      </div>
-      <div>
-        <Button color="primary" onClick={() => setAlertVisibility(true)}>
-          Button text
-        </Button>
+    <div className="wrapper">
+      <nav>
+        <Navbar />
+      </nav>
+      <div className="main-page">
+        <div className="sidebar">
+          <h1>Sidebar</h1>
+          <ListGroup
+            items={items}
+            heading="Filter"
+            onSelectItem={handleSelectItem}
+          />
+          <ListGroup
+            items={items}
+            heading="Filter"
+            onSelectItem={handleSelectItem}
+          />
+        </div>
+
+        <div className="content-page">
+          <h1>Post grid</h1>
+          <div>
+            {alertVisible && (
+              <Alert onClose={() => setAlertVisibility(false)}>Alert1!</Alert>
+            )}
+          </div>
+          <div>
+            <Button color="primary" onClick={() => setAlertVisibility(true)}>
+              Button text
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
